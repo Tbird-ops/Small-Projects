2e5bec5ebca245f73f07d535ad124fa6b94ffff0
Tristan Stapert <83035775+Tbird-ops@users.noreply.github.com>
2023-10-24 20:49:01 -0500

    Update ipgrabber.sh
    cleaner working state

--- ipgrabber/ipgrabber.sh
+++ ipgrabber/ipgrabber.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
-ipaddr=`hostname -I | awk '{print $1}'`;
-echo $ipaddr;
-
-curl
+ip4=$(/sbin/ip -o -4 addr list wlan0 | awk '{print $4}' | cut -d/ -f1)
+WEBHOOK={WEBHOOK HERE!!!!}
+curl \
+        -s \
+        -X POST \
+        -H "Content-Type: application/json" \
+        -d "{\"username\": \"cafe-pi\", \"content\": \"$ip4\"}" \
+        "$WEBHOOK"
